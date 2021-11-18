c741a1c820f0fb05b6681011a351b13d69f7f587
Megan Gronstad <mgronstad@g.ucla.edu>
2021-11-17 17:25:54 -0800

    Importing Unity Scene -- Speech Recognition

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(50, 50, 100, 30), "START"))
+        {
+            Debug.Log("Scene2 loading");
+            SceneManager.LoadScene("SpeechTest", LoadSceneMode.Single);
+        }
+    }
+}
---
+++ Assets/Scripts/LoadScene2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(50, 50, 120, 30), "Done with Speech"))
+        {
+            Debug.Log("Scene2 loading");
+            SceneManager.LoadScene("SpeechTestPt2", LoadSceneMode.Single);
+        }
+    }
+}
