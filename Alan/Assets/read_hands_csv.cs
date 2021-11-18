407f4534b91b0c73bf13bbc39231ab91b9dc329e
alanbdinh <91645866+alanbdinh@users.noreply.github.com>
2021-11-17 18:12:33 -0800

    changed r/w permissions to work w python

--- Alan/Assets/read_hands_csv.cs
+++ Alan/Assets/read_hands_csv.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System.Threading;
+
 
 // TODO: reader will need to retrieve the name of the python output .csv somehow. In the actual game, we might just make the name consistent.
 
@@ -10,38 +12,39 @@ public class read_hands_csv : MonoBehaviour
 {
 	public int x = -1;
 	public int y = -1;
-	String filename = "wrist_single.csv"
+	string filename = "PyOut/wrist_single.csv";
+	int delay_ms = 1000;
     // Start is called before the first frame update
     void Start()
+    {
+		Debug.Log("start");
+    }
+
+    // Update is called once per frame
+    void Update()
     {
 		// using example written here https://docs.microsoft.com/en-us/dotnet/api/system.io.streamreader?view=net-6.0
 		try
         {
             // Create an instance of StreamReader to read from a file.
             // The using statement also closes the StreamReader.
-            using (StreamReader sr = new StreamReader("TestFile.txt"))
+            using (StreamReader sr = new StreamReader(filename))
             {
                 string line;
                 // Read and display lines from the file until the end of
                 // the file is reached.
                 while ((line = sr.ReadLine()) != null)
                 {
-                    Console.WriteLine(line);
+                    Debug.Log(line);
                 }
             }
         }
         catch (Exception e)
         {
             // Let the user know what went wrong.
-            Console.WriteLine("The file could not be read:");
-            Console.WriteLine(e.Message);
+            Debug.Log("The file could not be read:");
+            Debug.Log(e.Message);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-		Debug.Log("Once per second")
-        Thread.Sleep(1000);
+        Thread.Sleep(delay_ms);
     }
 }
