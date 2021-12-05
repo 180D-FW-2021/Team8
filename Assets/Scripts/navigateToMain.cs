2506689c119a3bdd8606657a6ed3e10de3b4782d
Karalyn Sawchuk <sawchukk@gmail.com>
2021-12-05 14:27:27 -0800

    navigateToMain

---
+++ Assets/Scripts/navigateToMain.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class navigateToMain : MonoBehaviour
+{
+    public GameObject returnButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        returnButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void pressButton(){
+        SceneManager.LoadScene("MAINTEST", LoadSceneMode.Single);
+    }
+}
