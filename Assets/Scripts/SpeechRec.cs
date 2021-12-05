14c5277850f27c55fd45838a4a892048bbdc6349
Megan Gronstad <mgronstad@g.ucla.edu>
2021-12-05 12:39:58 -0800

    Added final changes to ingredient selection implementation

--- Assets/Scripts/SpeechRec.cs
+++ Assets/Scripts/SpeechRec.cs
@@ -18,59 +18,39 @@ namespace Speecher
         public StreamingRecognizer streamRec;
         //public fs_read_hands_csv handReader;
         public fs_read_hands_csv handReader;
+
         public GameObject includeText;
+        public GameObject excludeText;
+        public GameObject which;
 
         public GameObject addedText;
         public GameObject notAddedText;
 
+        public GameObject done;
+        public GameObject pointCtr;
+        public GameObject gameOver;
 
         public GameObject apple;
         public GameObject fish;
+        public GameObject grapefruit;
+        public GameObject cabbage;
+
         int i = 0;
         int j = 0;
-        int numOpts = 2;
+        //int k = 0;
+        int numOpts = 3;
         int points = 0;
         int missed = 0;
         int correct = 0;
+
+        //bool appleCompl = false;
+        //bool fishCompl = false;
 
         // Start is called before the first frame update
         void Start()
         {
             Debug.Log("Test");
-
-            //bool pos = CursorObject.fs_read_hands_csv.positioner;
-
-
-            //transcriptRec = streamRec.transcript;
         }
-
-
-            /*GameObject speechObj = GameObject.Find("SpeechTest");
-            StreamingRecognizer streamRec = speechObj.GetComponent<StreamingRecognizer>();
-            Debug.Log("Hello");
-            transcriptRec = streamRec.transcript;
-
-            //if (transcriptRec == "yes")
-            //{
-                Debug.Log("Hello");
-            }
-
-            Debug.Log(transcriptRec);
-            */
-
-            // IMPORTANT SHIT
-
-            //GameObject speechObj = GameObject.Find("SpeechTest");
-            //StreamingRecognizer streamRec = speechObj.GetComponent<StreamingRecognizer>();
-            //Console.WriteLine("Hello");
-            //transcriptRec = streamRec.transcript;
-            //Debug.Log(transcriptRec);
-
-
-            /*if (transcriptRec == "yes")
-            {
-                Debug.Log("Hello");
-            }*/
 
         // Update is called once per frame
         void Update()
@@ -80,7 +60,9 @@ namespace Speecher
             string[] recipe = { "Watermelon", "Canteloupe", "Apple", "Pear", "Orange", "Grapefruit", "Banana", "Coconut" };
             string[] ingredients = new string[5];
             string[] notIncluded = new string[5];
-            transcriptRec = streamRec.transcript;
+
+
+            //transcriptRec = streamRec.transcript;
 
             includeText.SetActive(false);
             addedText.SetActive(false);
@@ -89,20 +71,27 @@ namespace Speecher
             if (((cursy.position.x < apple.transform.position.x + 0.5) && (cursy.position.x > apple.transform.position.x - 0.5))
                     && ((cursy.position.z < apple.transform.position.z + 0.5) && (cursy.position.z > apple.transform.position.z - 0.5)))
             {
-                includeText.SetActive(true);
-                if (transcriptRec == "yes")
+                //streamRec.StartListening();
+                which.SetActive(true);
+                transcriptRec = streamRec.transcript;
+                if (transcriptRec == "include")
                 {
+                    includeText.SetActive(true);
                     ingredients[0] = apple.name;
                     i++;
                     addedText.SetActive(true);
                     apple.SetActive(false);
+                    Debug.Log("Added apple");
+                    //appleCompl = true;
                 }
-                else if (transcriptRec == "no")
+                else if (transcriptRec == "exclude")
                 {
-                    notIncluded[j] = apple.name;
+                    excludeText.SetActive(true);
+                    notIncluded[0] = apple.name;
                     j++;
                     notAddedText.SetActive(true);
                     apple.SetActive(false);
+                    //appleCompl = true;
                 }
 
                 //includeText.SetActive(false);
@@ -113,22 +102,96 @@ namespace Speecher
             if (((cursy.position.x < fish.transform.position.x + 0.3) && (cursy.position.x > fish.transform.position.x - 0.3))
                     && ((cursy.position.z < fish.transform.position.z + 0.7) && (cursy.position.z > fish.transform.position.z - 0.7)))
             {
-                includeText.SetActive(true);
-                if (transcriptRec == "yes")
+                which.SetActive(true);
+                //streamRec.StartListening();
+                transcriptRec = streamRec.transcript;
+                //includeText.SetActive(true);
+                if (transcriptRec == "include")
                 {
+                    includeText.SetActive(true);
                     ingredients[0] = fish.name;
                     i++;
                     addedText.SetActive(true);
                     fish.SetActive(false);
+                    Debug.Log("Added fish");
+                    //fishCompl = true;
                 }
-                else if (transcriptRec == "no")
+                else if (transcriptRec == "exclude")
                 {
-                    notIncluded[j] = fish.name;
+                    excludeText.SetActive(true);
+                    notIncluded[0] = fish.name;
                     j++;
                     notAddedText.SetActive(true);
                     fish.SetActive(false);
+                    //fishCompl = true;
                 }
                 // includeText.SetActive(false);
+            }
+
+            if (((cursy.position.x < grapefruit.transform.position.x + 0.3) && (cursy.position.x > grapefruit.transform.position.x - 0.3))
+                    && ((cursy.position.z < grapefruit.transform.position.z + 0.7) && (cursy.position.z > grapefruit.transform.position.z - 0.7)))
+            {
+                which.SetActive(true);
+                //streamRec.StartListening();
+                //Debug.Log("Grapefruit");
+                transcriptRec = streamRec.transcript;
+                //includeText.SetActive(true);
+                if (transcriptRec == "include")
+                {
+                    includeText.SetActive(true);
+                    ingredients[0] = grapefruit.name;
+                    i++;
+                    addedText.SetActive(true);
+                    grapefruit.SetActive(false);
+                    Debug.Log("Added grapefruit");
+                    //fishCompl = true;
+                }
+                else if (transcriptRec == "exclude")
+                {
+                    excludeText.SetActive(true);
+                    notIncluded[0] = grapefruit.name;
+                    j++;
+                    notAddedText.SetActive(true);
+                    grapefruit.SetActive(false);
+                    //fishCompl = true;
+                }
+                // includeText.SetActive(false);
+            }
+
+            if (((cursy.position.x < cabbage.transform.position.x + 0.5) && (cursy.position.x > cabbage.transform.position.x - 0.5))
+                    && ((cursy.position.z < cabbage.transform.position.z + 0.5) && (cursy.position.z > cabbage.transform.position.z - 0.5)))
+            {
+                which.SetActive(true);
+                //streamRec.StartListening();
+                transcriptRec = streamRec.transcript;
+                if (transcriptRec == "include")
+                {
+                    includeText.SetActive(true);
+                    ingredients[0] = cabbage.name;
+                    i++;
+                    addedText.SetActive(true);
+                    cabbage.SetActive(false);
+                    Debug.Log("Added cabbage");
+                    //appleCompl = true;
+                }
+                else if (transcriptRec == "exclude")
+                {
+                    excludeText.SetActive(true);
+                    notIncluded[0] = cabbage.name;
+                    j++;
+                    notAddedText.SetActive(true);
+                    cabbage.SetActive(false);
+                    //appleCompl = true;
+                }
+
+                //includeText.SetActive(false);
+            }
+
+            if (((cursy.position.x < done.transform.position.x + 1) && (cursy.position.x > done.transform.position.x - 1))
+                    && ((cursy.position.z < done.transform.position.z + 0.5) && (cursy.position.z > done.transform.position.z - 0.5)))
+            {
+                pointCtr.SetActive(true);
+                gameOver.SetActive(true);
             }
 
             //Debug.Log("Tester: " + transcriptRec);
