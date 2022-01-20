d45e65fa326e3c3d6797cf1c0df8990cbce881e3
Megan Gronstad <mgronstad@g.ucla.edu>
2022-01-19 18:04:45 -0800

    Transferring assets to canvas

---
+++ Assets/Scripts/PickMeScripts/HighScores.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoreEntry
+{
+    public string name;
+    public int score;
+}
+
+public class HighScores : MonoBehaviour
+{
+    public HighScoreDisplay[] highScoreDisplayArray;
+    List<HighScoreEntry> scores = new List<HighScoreEntry>();
+
+    void Start()
+    {
+        // Adds some test data
+        AddNewScore("John", 4500);
+        AddNewScore("Max", 5520);
+        AddNewScore("Dave", 380);
+        AddNewScore("Steve", 6654);
+        AddNewScore("Mike", 11021);
+        AddNewScore("Teddy", 3252);
+
+        UpdateDisplay();
+    }
+
+    void UpdateDisplay()
+    {
+        scores.Sort((HighScoreEntry x, HighScoreEntry y) => y.score.CompareTo(x.score));
+
+        for (int i = 0; i < highScoreDisplayArray.Length; i++)
+        {
+            if (i < scores.Count)
+            {
+                highScoreDisplayArray[i].DisplayHighScore(scores[i].name, scores[i].score);
+            }
+            else
+            {
+                highScoreDisplayArray[i].HideEntryDisplay();
+            }
+        }
+    }
+
+    void AddNewScore(string entryName, int entryScore)
+    {
+        scores.Add(new HighScoreEntry { name = entryName, score = entryScore });
+    }
+}
