caa892cbe5c248b9ed1bae9cff713844f8cbdaf9
Isabel Dominik <icd107@live.com>
2022-01-23 14:48:00 -0800

    Fixed error with tabs and spaces

--- GestureRecognitionIMUFiles/motionDetection.py
+++ GestureRecognitionIMUFiles/motionDetection.py
@@ -114,47 +114,47 @@ client.publish('ece180d/team8/imu', "Test message", qos=1)
 # Accelerometer calibration
 
 def accel_fit(x_input,m_x,b):
-    return (m_x*x_input)+b # fit equation for accel calibration
+	return (m_x*x_input)+b # fit equation for accel calibration
 
 def get_accel():
-    ax = IMU.readACCx()
+	ax = IMU.readACCx()
 	ay = IMU.readACCy()
 	az = IMU.readACCz()
-    return ax,ay,az
-    
+	return ax,ay,az
+	
 def accel_cal():
-    print("-"*50)
-    print("Accelerometer Calibration")
-    mpu_offsets = [[],[],[]] # offset array to be printed
-    axis_vec = ['z','y','x'] # axis labels
-    cal_directions = ["upward","downward","perpendicular to gravity"] # direction for IMU cal
-    cal_indices = [2,1,0] # axis indices
-    for qq,ax_qq in enumerate(axis_vec):
-        ax_offsets = [[],[],[]]
-        print("-"*50)
-        for direc_ii,direc in enumerate(cal_directions):
-            #input("-"*8+" Press Enter and Keep IMU Steady to Calibrate the Accelerometer with the -"+\
-            #  ax_qq+"-axis pointed "+direc)
-            [mpu6050_conv() for ii in range(0,cal_size)] # clear buffer between readings
-            mpu_array = []
-            while len(mpu_array)<cal_size:
-                try:
-                    ax,ay,az = get_accel()
-                    mpu_array.append([ax,ay,az]) # append to array
-                except:
-                    continue
-            ax_offsets[direc_ii] = np.array(mpu_array)[:,cal_indices[qq]] # offsets for direction
+	print("-"*50)
+	print("Accelerometer Calibration")
+	mpu_offsets = [[],[],[]] # offset array to be printed
+	axis_vec = ['z','y','x'] # axis labels
+	cal_directions = ["upward","downward","perpendicular to gravity"] # direction for IMU cal
+	cal_indices = [2,1,0] # axis indices
+	for qq,ax_qq in enumerate(axis_vec):
+		ax_offsets = [[],[],[]]
+		print("-"*50)
+		for direc_ii,direc in enumerate(cal_directions):
+			#input("-"*8+" Press Enter and Keep IMU Steady to Calibrate the Accelerometer with the -"+\
+			#  ax_qq+"-axis pointed "+direc)
+			[mpu6050_conv() for ii in range(0,cal_size)] # clear buffer between readings
+			mpu_array = []
+			while len(mpu_array)<cal_size:
+				try:
+					ax,ay,az = get_accel()
+					mpu_array.append([ax,ay,az]) # append to array
+				except:
+					continue
+			ax_offsets[direc_ii] = np.array(mpu_array)[:,cal_indices[qq]] # offsets for direction
 
-        # Use three calibrations (+1g, -1g, 0g) for linear fit
-        popts,_ = curve_fit(accel_fit,np.append(np.append(ax_offsets[0],
-                                 ax_offsets[1]),ax_offsets[2]),
-                   np.append(np.append(1.0*np.ones(np.shape(ax_offsets[0])),
-                    -1.0*np.ones(np.shape(ax_offsets[1]))),
-                        0.0*np.ones(np.shape(ax_offsets[2]))),
-                            maxfev=10000)
-        mpu_offsets[cal_indices[qq]] = popts # place slope and intercept in offset array
-    print('Accelerometer Calibrations Complete')
-    return mpu_offsets
+		# Use three calibrations (+1g, -1g, 0g) for linear fit
+		popts,_ = curve_fit(accel_fit,np.append(np.append(ax_offsets[0],
+								ax_offsets[1]),ax_offsets[2]),
+								np.append(np.append(1.0*np.ones(np.shape(ax_offsets[0])),
+								-1.0*np.ones(np.shape(ax_offsets[1]))),
+								0.0*np.ones(np.shape(ax_offsets[2]))),
+								maxfev=10000)
+		mpu_offsets[cal_indices[qq]] = popts # place slope and intercept in offset array
+	print('Accelerometer Calibrations Complete')
+	return mpu_offsets
 
 
 
@@ -220,10 +220,6 @@ while True:
 			# print("Back!")
 			# movements.append("B")
 			# time.sleep(cooldown)
-
-		# Square recognition (RDLU)
-		# shape = movements[-4:]
-		# move = movements[-1:]
 
 		if move == pure_square[shape_stage]:
 			print("Square motion found")
