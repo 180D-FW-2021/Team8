1c17adf2d48f9389e763f2ee7a04b2c94537d8bb
sawchukk <sawchukk@gmail.com>
2022-02-01 14:08:15 -0800

    Auto stash before merge of "main" and "origin/main"

--- Library/PackageCache/com.oshoham.unity-google-cloud-streaming-speech-to-text@0.1.8/Runtime/StreamingRecognizer.cs
+++ Library/PackageCache/com.oshoham.unity-google-cloud-streaming-speech-to-text@0.1.8/Runtime/StreamingRecognizer.cs
@@ -62,8 +62,11 @@ namespace GoogleCloudStreamingSpeechToText {
         private const float MicInitializationTimeout = 1;
         private const int StreamingLimit = 290000; // almost 5 minutes
 
+<<<<<<< Updated upstream
         public string transcript;
 
+=======
+>>>>>>> Stashed changes
         public void StartListening() {
             if (!_initialized) {
                 return;
@@ -241,7 +244,11 @@ namespace GoogleCloudStreamingSpeechToText {
 
                 _resultEndTime = (int)((result.ResultEndTime.Seconds * 1000) + (result.ResultEndTime.Nanos / 1000000));
 
+<<<<<<< Updated upstream
                 transcript = result.Alternatives[0].Transcript.Trim();
+=======
+                string transcript = result.Alternatives[0].Transcript.Trim();
+>>>>>>> Stashed changes
 
                 if (result.IsFinal) {
                     if (enableDebugLogging) {
