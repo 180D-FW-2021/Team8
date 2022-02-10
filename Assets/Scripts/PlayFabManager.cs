093a3135326da1a102ed5bd59e5fec7f3c1d1836
sawchukk <sawchukk@gmail.com>
2022-02-09 17:14:12 -0800

    Auto stash before merge of "main" and "origin/main"
    New navigation UI

---
+++ Assets/Scripts/PlayFabManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class PlayFabManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Login(){
+       // var request = new LoginWithCustomIDRequest{
+        //    CustomID = SystemInfo.deviceUniqueIdentifier, CreateAccount = true
+        //};
+
+    }
+}
