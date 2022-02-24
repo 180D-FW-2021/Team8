03804ce663d0cfd8873a7832263a4eeb86fb73e7
sawchukk <sawchukk@gmail.com>
2022-02-23 17:08:29 -0800

    lighting edits

---
+++ Assets/Scripts/PlayFabManager1.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class PlayFabManager1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Login();
+    }
+
+    // Update is called once per frame
+    void Login()
+    {
+        var request = new LoginWithCustomIDRequest {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+    }
+
+    void OnSuccess(LoginResult result){
+        Debug.Log("Successful login/account create!");
+    }
+
+    void OnError(PlayFabError error){
+        Debug.Log("Error while logging in/creating account");
+        Debug.Log(error.GenerateErrorReport());
+    }
+    
+    public void SendLeaderboard(int score){
+        var request = new UpdatePlayerStatisticsRequest {
+            Statistics = new List<StatisticUpdate> {
+                new StatisticUpdate {
+                    StatisticName = "TiltLeaderboard",
+                    Value = score
+                }
+            }
+        };
+        PlayFabClientAPI.UpdatePlayerStatistics(request, OnLeaderboardUpdate, OnError);
+
+    }
+
+    void OnLeaderboardUpdate(UpdatePlayerStatisticsResult result){
+        Debug.Log("Successful leaderboard sent");
+    }
+
+    public void GetLeaderboard(){
+        var request = new GetLeaderboardRequest{
+            StatisticName = "TiltLeaderboard",
+            StartPosition = 0,
+            MaxResultsCount = 3
+        };
+        PlayFabClientAPI.GetLeaderboard(request, OnLeaderboardGet, OnError);
+    }
+
+    void OnLeaderboardGet(GetLeaderboardResult result){
+        foreach (var item in result.Leaderboard){
+            Debug.Log(item.Position + " " + item.PlayFabId + " " + item.StatValue);
+        }
+    }
+}
