ff0c513e81a55184ac314a19db762e5ef722379c
Isabel Dominik <icd107@live.com>
2022-03-06 11:53:00 -0800

    Adding in comments

--- Assets/Scripts/ChoppingScripts/fryingPanMovement.cs
+++ Assets/Scripts/ChoppingScripts/fryingPanMovement.cs
@@ -41,6 +41,7 @@ public class fryingPanMovement : MonoBehaviour
         //this.transform.Rotate(xTilt,yTilt,0.0f);
         this.transform.rotation = Quaternion.Euler(xTilt * tiltScale, 0.0f, yTilt * tiltScale);
 
+        // A for loop as a delay to space out the tilting updates to not crash Unity
         for(int i = 0; i < 10; i++) {}
     }
 
