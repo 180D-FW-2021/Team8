bfa1cab4feccfbac457dab76c6df995953f659d2
Isabel Dominik <icd107@live.com>
2022-03-10 14:13:17 -0800

    Working on "play again" button

--- Assets/Scripts/MenuScripts/MainMenuTransition.cs
+++ Assets/Scripts/MenuScripts/MainMenuTransition.cs
@@ -43,4 +43,10 @@ public class MainMenuTransition : MonoBehaviour
         }
         
     }
+
+    public void loadFryFry()
+    {
+        Debug.Log("Fry Fry loading");
+        SceneManager.LoadScene("Fry Fry", LoadSceneMode.Single);
+    }
 }
